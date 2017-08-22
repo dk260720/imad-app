df7e178578eb870aefc4c3dfff43c4f725af61ba
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-22 20:04:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,6 +54,9 @@ function createTemplate (data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/:articleName',function (req, res){
     var articleName = req.params.articleName;
