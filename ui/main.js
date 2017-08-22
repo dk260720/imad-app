b4d9854b7d8fcb6d6c5f6336d132137cced5b9c1
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-22 21:49:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img = document.getElementByid('madi');
-img.onclick = function () {
-    img.style.marginleft= '100px';
+var marginleft = 0;
+function moveRight (){
+marginleft = marginleft+5;
+img.style.marginleft = marginleft+px;    
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
 };
