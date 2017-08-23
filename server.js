cc7ade3e2141976b4a7b894a1bb1a28bc9310a42
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-23 21:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 app.use(morgan('combined'));
+var counter=0;
+app.get('/counter',function(req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+    });
 var articles = {
              'article-one' : {
                title: 'dinesh kumar |  feeling cool',
@@ -66,11 +71,7 @@ app.get('/:articleName',function (req, res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-var counter=0;
-app.get('/counter',function(req, res){
-    counter=counter+1;
-    res.send(counter.toString());
-    });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
