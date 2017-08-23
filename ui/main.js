7a60ee5887420de57c119d9d4521c69c5f22b030
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-23 22:12:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
 console.log('Loaded!');
-var img = document.getElementByid('madi');
-var marginLeft = 0;
-function moveRight (){
-marginLeft = marginLeft + 5;
-img.style.marginLeft = marginLeft + 'px';    
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+var counter= document.getElementById('counter')
+var counter=0;
+button.onclick= function(){
+counter=counter+1;
+var span= document.getElementById('count')
+span.InnerHTML= counter.toString();
 };
