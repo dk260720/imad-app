fa6703ccebb348566bce1543b7a68af6a9269b6d
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-24 22:39:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,23 +14,27 @@ request.open('GET','http://dk260720.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 var submit= document.getElementById('submit_btn');
-submit.onclick = function (){
-    var request = new XMLHttpRequest();
-request.onreadystatechange  = function (){
-      if(request.readyState ===XMLHttpRequest.DONE) {
-          if(request.status===200){
-var names=request.responseText;
-names=JSON.parse(names);
-var list='';
-for(var i=0; i<names.length;i++)
+submit.onclick = function ()
 {
-  list+='<li>' + names[i] + '</li>';
-}
-var ul=document.getElementById('namelist');
-ul.innerHTML=list;
-}
- }
-};
+    var request = new XMLHttpRequest();
+    request.onreadystatechange  = function ()
+    {
+        if(request.readyState ===XMLHttpRequest.DONE)
+        {
+          if(request.status===200) 
+          {
+          var names=request.responseText;
+          names=JSON.parse(names);
+          var list='';
+          for(var i=0; i<names.length;i++)
+          {
+          list+='<li>' + names[i] + '</li>';
+          }
+          var ul=document.getElementById('namelist');
+          ul.innerHTML=list;
+          }
+        }
+    };
 var nameinput= document.getElementById('name');
 var name= nameInput.value;
 request.open('GET','http://dk260720.imad.hasura-app.io/submitname?name=' + name, true);
