7ccdccf38918df8356b7989fa8e7a6564e4180b8
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-24 21:18:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,16 +13,22 @@ request.onreadystatechange  = function(){
           }
       } 
     };
-request.open('GET','http://dk260720.imad.hasura-app.io/counter', true);
+request.open('GET','http://dk260720.imad.hasura-app.io/submit-name?name='+name, true);
 request.send(null);
     
 };
 
 var nameinput= document.getElementById('name');
-var name= nameinput.value;
+var name= nameInput.value;
 var submit= document.getElementById('submit');
 submit.onclick= function(){
-var names=['name1','name2','name3','name4'];
+    var request = new XMLHttpRequest();
+
+request.onreadystatechange  = function(){
+      if(request.readyState ===XMLHttpRequest.DONE) {
+          if(request.status===200){
+var names=request.responseText;
+names=JSON.parse(names);
 var list='';
 for(var i=0; i<names.length;i++)
 {
@@ -30,4 +36,10 @@ for(var i=0; i<names.length;i++)
 }
 var ul=document.getElementById('namelist');
 ul.innerHTML=list;
+}
+ }
+};
+request.open('GET','http://dk260720.imad.hasura-app.io/submit-name?name='+name, true);
+request.send(null);
+    
 };
