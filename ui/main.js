3cfc72cc2f3c8fcab2aa0c895e26ce33d235aa2c
dinesh kumar amaranathan <30660305+dk260720@users.noreply.github.com>
2017-08-24 22:20:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button= document.getElementById('counter');
 button.onclick= function () {
     var request = new XMLHttpRequest();
 
-request.onreadystatechange  = function(){
+request.onreadystatechange  = function (){
       if(request.readyState ===XMLHttpRequest.DONE) {
           if(request.status===200){
               var counter = request.responseText;
@@ -22,7 +22,7 @@ var submit= document.getElementById('submit_btn');
 submit.onclick= function(){
     var request = new XMLHttpRequest();
 
-request.onreadystatechange  = function(){
+request.onreadystatechange  = function (){
       if(request.readyState ===XMLHttpRequest.DONE) {
           if(request.status===200){
 var names=request.responseText;
